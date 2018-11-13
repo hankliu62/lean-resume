740ffc5d6ae46ede6e34a439ba47bf9ab1fdad48
hankliu <397694072@qq.com>
2018-11-13 19:47:45 +0800

    feat(resume): remove config keys

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const AV = require('leanengine');
 const createApp = require('./server/app');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID || 'esuJQabE1I7kThJfoWIjc04p-gzGzoHsz',
-  appKey: process.env.LEANCLOUD_APP_KEY || 'XTCvvn8sXhQpXhQkYS8ijkOY',
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'Mle0PB314voXGprTHKpioIu0'
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY,
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
-
-
 const isDev = process.env.NODE_ENV !== 'production';
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
-const port = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+const port = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000, 10);
 
 createApp()
   .then((app) => { http.createServer(app.callback()).listen(port); })
